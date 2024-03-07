9438edb91daea4218c1d6eff6b80ae069dce6ce8
Corso Montuori <137323676+Corsionio@users.noreply.github.com>
2024-03-07 00:02:10 -0500

    Update values.py
    Added redo request upon a server timeout

--- values.py
+++ values.py
@@ -16,7 +16,7 @@ client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager, requests_timeout = 10, retries = 10)
 
 
 def get_token():
