a8c10ad85e3fcc0cf68ade341702252b19d23148
Corso Montuori <137323676+Corsionio@users.noreply.github.com>
2024-03-29 21:54:45 -0400

    Add files via upload

--- values.py
+++ values.py
@@ -5,7 +5,6 @@
 from dotenv import load_dotenv
 import os
 import base64
-from requests import post
 import json
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -16,7 +15,7 @@ client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager, requests_timeout = 10, retries = 10)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 
 def get_token():
